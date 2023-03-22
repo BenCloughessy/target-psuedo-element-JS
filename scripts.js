fd0328a5d86086308ce3b2c2c05b6c4e617d4f42
BenCloughessy <110562848+BenCloughessy@users.noreply.github.com>
2023-03-22 08:13:30 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const container1 = document.querySelector(".container1")
+const container2 = document.querySelector(".container2")
+
+container1.addEventListener("click", () => {
+    container1.style.backgroundColor = "rgb(50, 117, 219)"
+})
+
+container2.addEventListener("click", () => {
+    container2.style.backgroundColor = "rgb(50, 117, 219)"
+})
